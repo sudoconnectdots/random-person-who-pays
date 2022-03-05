44e48884651c79ac9bc43aecef7df651334e7abb
sudoconnectdots <98963561+sudoconnectdots@users.noreply.github.com>
2022-03-05 14:58:37 +0100

    Add files via upload

---
+++ random_selection_pay.py
@@ -0,0 +1,20 @@
+import random
+
+names_string = input("Give me  names, seperated by a comma , ")
+names = names_string.split(",")
+
+num_items = len(names)
+#Generate random numbers between 0 and the last index. 
+random_choice = random.randint(0, num_items - 1)
+#Pick out random person from list of names using the random number.
+person_who_will_pay = names[random_choice]
+
+print(person_who_will_pay + " is going to buy the meal today! god bless this is your day  :)")
+
+
+
+
+
+
+
+
